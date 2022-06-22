3dd780f15f305fa4e926366cd64013558f7462e8
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-06-22 00:04:14 -0300

    include tests in cidadaoroutes

--- test/functional/cidadao.test.ts
+++ test/functional/cidadao.test.ts
@@ -1,44 +1,97 @@
 import supertest from 'supertest';
 import { app } from '../../src/server';
 
-describe('Beach forecast functional tests', () => {
-  it('should return a forecast with just a few times', async () => {
+const cidadao_create_Mock = {
+  "name": "Teste Create",
+  "email": "test_03@teste.com",
+  "celular": "219999999975",
+  "senha": "12345678",
+  "nick_name": "Teste_nick3",
+  "sexo": "Masculino"
+}
+
+const sutFactoryGetAll = async () => {
+  const { body, status } = await supertest(app).get('/cidadao/');
+  return body
+}
+
+const idDeleteMock = 'e1e1e071-3f4a-4570-9184-1b89e12cce3e'
+const idMock = '73dda17d-dd32-470a-8dde-b9518b4dcf1a';
+const idErrorMock = '73dda17d-dd32-470a-8dde-b9518b4dcf1';
+
+const sutFactoryGet = async () => {
+  const { body, status } = await supertest(app).get(`/cidadao/${idMock}`);
+  return body
+}
+
+const sutFactoryCreate = async () => {
+  const { body, status } = await supertest(app).post(`/cidadao/`);
+  return body
+}
+
+describe('Get all cidadao route test', () => {
+  it('should return an array with all citizens', async () => {
     const { body, status } = await supertest(app).get('/cidadao/');
+    const expectedGetall = await sutFactoryGetAll()
     expect(status).toEqual(200);
-    expect(body).toEqual([
-      {
-        id: '73dda17d-dd32-470a-8dde-b9518b4dcf1a',
-        name: 'Rômulo Rodrigues de Oliveira',
-        email: 'romulo_rodrigues@teste.com',
-        celular: '21999999999',
-        senha: '12345678',
-        nick_name: 'Romin_',
-        sexo: 'Masculino',
-        create_at: '2022-06-20T20:47:39.712Z',
-        updated_at: '2022-06-20T20:54:33.185Z',
-      },
-      {
-        id: 'bb240b72-8a98-453a-b030-f92154318076',
-        name: 'Élissa Santos de Oliveira',
-        email: 'elissa_santos@teste.com',
-        celular: '21999999998',
-        senha: '12345678',
-        nick_name: 'Lise_',
-        sexo: 'Feminino',
-        create_at: '2022-06-20T21:06:12.005Z',
-        updated_at: '2022-06-20T21:06:12.005Z',
-      },
-      {
-        id: '69572845-2679-4e80-8971-4b647f47adb6',
-        name: 'Julio Cesar',
-        email: 'julio_cesar@teste.com',
-        celular: '21999999997',
-        senha: '12345678',
-        nick_name: 'Julinho',
-        sexo: 'Masculino',
-        create_at: '2022-06-20T21:42:53.644Z',
-        updated_at: '2022-06-20T21:42:53.670Z',
-      },
-    ]);
+    expect(body).toEqual(expectedGetall);
   });
+});
+
+/* describe('Delete cidadao route test', () => {
+  it('should return an array with all citizens', async () => {
+    const { body, status } = await supertest(app).get('/cidadao/');
+    const expectedGetall = await sutFactoryGetAll()
+    expect(status).toEqual(200);
+    expect(body).toEqual(expectedGetall);
+  });
+}); */
+
+describe('Get cidadao route test', () => {
+
+  it('should return an citizen', async () => {
+    const { body, status } = await supertest(app).get(`/cidadao/${idMock}`);
+    const expectedGet = await sutFactoryGet()
+    expect(status).toEqual(200);
+    expect(body).toEqual(expectedGet);
+  });
+
+  it('should return error when an citizen does not exist', async () => {
+    const { body, status } = await supertest(app).get(`/cidadao/${idErrorMock}`);
+    expect(status).toEqual(404);
+    expect(body).toEqual({
+      'message': 'Cidadão does not exists',
+      'status': 'error'
+    });
+  });
+});
+
+describe('Create cidadao route test', () => {
+
+  it('should return an citizen create', async () => {
+    const { body, status } = await supertest(app).post(`/cidadao/`).send(cidadao_create_Mock);
+    expect(status).toEqual(201);
+    expect(body).toEqual({
+      "id": body.id,
+      "name": "Teste Create",
+      "email": "test_03@teste.com",
+      "celular": "219999999975",
+      "senha": "12345678",
+      "nick_name": "Teste_nick3",
+      "sexo": "Masculino",
+      "create_at": body.create_at,
+      "updated_at": body.updated_at
+    });
+
+    const deleteCidadaoMock = await supertest(app).delete(`/cidadao/${body.id}`)
+  });
+
+  /* it('should return error when an citizen does not exist', async () => {
+    const { body, status } = await supertest(app).post(`/cidadao/${idErrorMock}`);
+    expect(status).toEqual(404);
+    expect(body).toEqual({
+      'message': 'Cidadão does not exists',
+      'status': 'error'
+    });
+  }); */
 });
