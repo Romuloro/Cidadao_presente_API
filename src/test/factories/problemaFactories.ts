b56400dba3fd01afb26d870abdccd462afa8a53c
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-07-26 14:14:08 -0300

    test: fix problema test and fix problemaFactories

--- src/test/factories/problemaFactories.ts
+++ src/test/factories/problemaFactories.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker';
 
 export const Tipo = ["Infraestrutura", "Saude", "Poluicao", "Seguranca"]
 
-export const comentario_create_Mock = {
+export const problema_create_Mock = {
     descricao: faker.commerce.productDescription(),
     titulo: faker.commerce.productDescription(),
     tipo: Tipo[Math.floor((Math.random() * Tipo.length))],
