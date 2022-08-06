11b2a74b2317bdbea47340dd63107747d8bf54cc
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-06 15:45:59 -0300

    feat: problema and comentarios in get all posts

--- src/modules/post/useCase/getAllPost/GetAllPostUseCase.ts
+++ src/modules/post/useCase/getAllPost/GetAllPostUseCase.ts
@@ -3,7 +3,30 @@ import { prisma } from '../../../../prisma/client';
 
 export class GetAllPostUseCase {
   async execute(): Promise<Post[]> {
-    const post = await prisma.post.findMany({});
+    const post = await prisma.post.findMany({
+      include: {
+        problemas: {
+          select: {
+            titulo: true,
+            tipo: true
+          }
+        },
+        comentarios: {
+          orderBy: {
+            create_at: "desc"
+          },
+          select: {
+            descricao: true,
+            tipo: true,
+            author: {
+              select: {
+                nick_name: true
+              }
+            }
+          }
+        }
+      }
+    });
 
     return post;
   }
